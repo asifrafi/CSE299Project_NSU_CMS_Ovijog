71d0c3048d54ddfb1751257a69d0964e8d98f4b2
Seum <monirul.seum@northsouth.edu>
2023-03-21 11:53:22 +0600

    Status In complain Model Added

--- models/complain-model.js
+++ models/complain-model.js
@@ -1,58 +1,55 @@
 const mongoose = require('mongoose');
 const User = require('./user-model');
 
-const complainSchema = new mongoose.Schema(
-  {
+const complainSchema = new mongoose.Schema({
     title: {
-      type: String,
+        type: String,
     },
     complain: {
-      type: String,
+        type: String,
     },
     evidence: {
-      type: Array,
+        type: Array,
     },
     createdAt: {
-      type: Date,
-      default: Date.now,
+        type: Date,
+        default: Date.now,
     },
     complainer: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
     },
     faulty: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
     },
     reviewer: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
     },
-    comments: [
-      {
+    comments: [{
         _id: { type: String },
         commenter: { type: mongoose.Schema.ObjectId, ref: 'User' },
         text: { type: String },
         date: { type: Date, default: Date.now },
-      },
-    ],
-    versions: [
-      {
+    }, ],
+    status: { type: String, default: 'Open' },
+    statusComment: { type: String },
+    statusCloser: { type: mongoose.Schema.ObjectId, ref: 'User' },
+    statusDate: { type: Date },
+    versions: [{
         _id: { type: mongoose.Schema.ObjectId, ref: 'ComplainHistory' },
         editor: { type: mongoose.Schema.ObjectId, ref: 'User' },
         date: { type: Date, default: Date.now },
-      },
-    ],
+    }, ],
     versionNumber: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
-  },
-  {
+}, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-  }
-);
+});
 
 const Complain = mongoose.model('Complain', complainSchema);
 
